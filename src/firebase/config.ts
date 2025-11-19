68adedac303c274f9b787cecdb5861f9c8941c72
Shivam Narkar <shivam.narkar@ustu.edu.in>
2025-11-19 19:25:06 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3793875319-a9935",
+  "appId": "1:905575216290:web:fcd38450551f51b66593fb",
+  "apiKey": "AIzaSyC4deJ6wLnq1d6ExgZZRY0C9AAAS6zIM_0",
+  "authDomain": "studio-3793875319-a9935.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "905575216290"
+};
